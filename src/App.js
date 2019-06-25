8aaab5d36f511015bce2e5efa25dfa4c810303b3
Anurag Bajaj <anuragb26@gmail.com>
2019-06-25 16:38:17 +0530

    Chore:UI for App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { withAuthenticator } from "aws-amplify-react";
 
 function App() {
-  return <div>App</div>;
+  const [notes, setNotes] = useState([{ id: 1, note: "Hello world" }]);
+  return (
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-l">Amplify note-taker</h1>
+      <form className="mb-3">
+        <input type="text" className="pa2 f4" placeholder="Write your note" />
+        <button className="pa2 f4" type="submit">
+          Add Note
+        </button>
+      </form>
+      <div>
+        {notes.map(item => (
+          <div key={item.id} className="flex items-center">
+            <li className="pa1 f3">{item.note}</li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
